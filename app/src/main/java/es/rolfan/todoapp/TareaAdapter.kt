42f42456c869f7b08354e17e110027af02b15e57
Aimar Ibarra <aimar_ibarra@proton.me>
2024-10-23 14:22:03 +0200

    button cambiado a imagen

--- app/src/main/java/es/rolfan/todoapp/TareaAdapter.kt
+++ app/src/main/java/es/rolfan/todoapp/TareaAdapter.kt
@@ -3,14 +3,14 @@ package es.rolfan.todoapp
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 
 class TareaAdapter(private val tareas: MutableList<String>) : RecyclerView.Adapter<TareaAdapter.ViewHolder>() {
     class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
         val textView: TextView = view.findViewById(R.id.texto)
-        val hecho: Button = view.findViewById(R.id.hecho)
+        val hecho: ImageView = view.findViewById(R.id.hecho)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
