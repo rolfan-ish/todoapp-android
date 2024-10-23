b5c2a35039c381cee1543ccf9a2373a15fc4d40a
Aimar Ibarra <aimar_ibarra@proton.me>
2024-10-23 13:07:14 +0200

    accion añadida

--- app/src/main/java/es/rolfan/todoapp/MainActivity.kt
+++ app/src/main/java/es/rolfan/todoapp/MainActivity.kt
@@ -1,6 +1,8 @@
 package es.rolfan.todoapp
 
 import android.os.Bundle
+import android.util.Log
+import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -17,4 +19,10 @@ class MainActivity : AppCompatActivity() {
             insets
         }
     }
+
+    fun agregarTarea(view: View) {
+        Log.i("TAREAS", "Añadir tarea pulsado")
+    }
+
+
 }
