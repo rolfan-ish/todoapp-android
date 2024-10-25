d66897cca3fa3d10595adfe5c66129d301c60d04
Aimar Ibarra <aimar_ibarra@proton.me>
2024-10-25 13:16:37 +0200

    mejora 1

--- app/src/main/java/es/rolfan/todoapp/MainActivity.kt
+++ app/src/main/java/es/rolfan/todoapp/MainActivity.kt
@@ -1,8 +1,8 @@
 package es.rolfan.todoapp
 
 import android.os.Bundle
-import android.view.View
-import android.widget.TextView
+import android.widget.Button
+import android.widget.EditText
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -12,23 +12,23 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
-
-    lateinit var prefs: Preferencias
-    lateinit var listaTareas: RecyclerView
-    lateinit var tarea: TextView
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
 
         // Inicializar
-        prefs = Preferencias(this)
-        tarea = findViewById(R.id.tarea)
-        val adapter = TareaAdapter(prefs.read())
-        listaTareas = findViewById(R.id.listaTareas)
+        val tarea: EditText = findViewById(R.id.tarea)
+        val adapter = TareaAdapter(Preferencias.read(this))
+        val listaTareas: RecyclerView = findViewById(R.id.listaTareas)
         listaTareas.layoutManager = LinearLayoutManager(this)
         listaTareas.adapter = adapter
+
+        findViewById<Button>(R.id.agregar).setOnClickListener {
+            if (!adapter.add(tarea.text.toString()))
+                Toast.makeText(this, "El elemento ya existe", Toast.LENGTH_SHORT)
+                    .show()
+        }
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
@@ -39,14 +39,8 @@ class MainActivity : AppCompatActivity() {
 
     override fun onPause() {
         super.onPause()
+        val listaTareas: RecyclerView = findViewById(R.id.listaTareas)
         val adapter = listaTareas.adapter as TareaAdapter
-        prefs.write(adapter.tareas)
-    }
-
-    fun agregarTarea(view: View) {
-        val adapter = listaTareas.adapter as TareaAdapter
-        if (!adapter.add(tarea.text.toString()))
-            Toast.makeText(this, "El elemento ya existe", Toast.LENGTH_SHORT)
-                .show()
+        Preferencias.write(this, adapter.tareas)
     }
 }
--- app/src/main/java/es/rolfan/todoapp/Preferencias.kt
+++ app/src/main/java/es/rolfan/todoapp/Preferencias.kt
@@ -3,26 +3,19 @@ package es.rolfan.todoapp
 import android.app.Activity
 import android.content.Context
 
-class Preferencias(ctx: Activity) {
+private const val TASKS_KEY = "tasks"
+
+class Preferencias {
     companion object {
-        const val TASKS_KEY = "tasks"
-    }
+        fun read(ctx: Activity) =
+            LinkedHashSet(ctx.getPreferences(Context.MODE_PRIVATE)
+                .getStringSet(TASKS_KEY, setOf()))
 
-    private val prefs = ctx.getPreferences(Context.MODE_PRIVATE);
-
-    // Siguiendo el contrato, sabemos que este valor nunca sera null
-    fun read() = prefs.getStringSet(TASKS_KEY, linkedSetOf())!! as LinkedHashSet<String>
-
-    fun write(tasks: LinkedHashSet<String>) {
-        // La palabra cable `with` es azucar, en este caso tiene el siguiente comportamiento:
-        // ```
-        // val editor = prefs.edit()
-        // editor.putStringSet(TASKS_KEY, tasks)
-        // editor.apply()
-        // ```
-        with (prefs.edit()) {
-            putStringSet(TASKS_KEY, tasks)
-            apply()
+        fun write(ctx: Activity, tasks: LinkedHashSet<String>) {
+            with (ctx.getPreferences(Context.MODE_PRIVATE).edit()) {
+                putStringSet(TASKS_KEY, tasks)
+                apply()
+            }
         }
     }
 }
