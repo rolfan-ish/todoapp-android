7fbd44e61fc16bbc49465351fcf12d5802521e4b
Aimar Ibarra <aimar_ibarra@proton.me>
2024-10-30 13:35:12 +0100

    javadoc

--- app/src/main/java/es/rolfan/todoapp/Preferencias.kt
+++ app/src/main/java/es/rolfan/todoapp/Preferencias.kt
@@ -5,12 +5,23 @@ import android.content.Context
 
 private const val TASKS_KEY = "tasks"
 
+/**
+ * Clase de utilidad para leer preferencias
+ */
 class Preferencias {
     companion object {
+        /**
+         * @param ctx La actividad de la que se quieren sacar preferencias
+         * @return set de tareas hechas
+         */
         fun read(ctx: Activity) =
             LinkedHashSet(ctx.getPreferences(Context.MODE_PRIVATE)
                 .getStringSet(TASKS_KEY, setOf()))
 
+        /**
+         * @param ctx La actividad donde se quieren guardar preferencias
+         * @param tasks Las preferencias que se quieren guardar
+         */
         fun write(ctx: Activity, tasks: LinkedHashSet<String>) {
             with (ctx.getPreferences(Context.MODE_PRIVATE).edit()) {
                 putStringSet(TASKS_KEY, tasks)
